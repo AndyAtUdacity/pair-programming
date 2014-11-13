516c00b9695d00360aa1ae64734518ab06770797
Andy Brown <pursuingpareto@gmail.com>
2014-11-13 14:56:57 -0800

    remove Discussions panel

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,6 +1,6 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.querySelector(".CodeMirror")) {
+	if (document.querySelector(".CodeMirror") && $('div.col-xs-3:contains("iscussions")')) {
 		clearInterval(readyStateCheckInterval);
 
 		// ----------------------------------------------------------
@@ -9,11 +9,21 @@ chrome.extension.sendMessage({}, function(response) {
 		// ----------------------------------------------------------
 
 		var containerElem = document.querySelector(".CodeMirror");
-
+		var discussionsElemRemoved = false;
+		var codeTipsDiv;
 		var observer = new MutationObserver (function(mutations) {
 			// do stuff
+			if (!discussionsElemRemoved){
+				var headings = $('h3:contains("iscussions")');
+				var discussionsHeading = headings[0];
+				codeTipsDiv = $(discussionsHeading).parent()
+				$(codeTipsDiv).empty();
+				$(codeTipsDiv).attr('id','code-tips');
+				$(codeTipsDiv).append('<h3>Python Tips</h3>');
+			}
+
 			$('.CodeMirror-lines pre').each(function(i, elem){
-				console.log($(elem).text());
+				// console.log($(elem).text());
 			});
 
 			// console.log('Hi!', mutations);
