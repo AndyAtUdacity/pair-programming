0601f00176afddcdf17d4671b2303fbec8b87563
Andy Brown <pursuingpareto@gmail.com>
2014-11-13 15:59:18 -0800

    example python tips

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -20,6 +20,9 @@ chrome.extension.sendMessage({}, function(response) {
 				$(codeTipsDiv).empty();
 				$(codeTipsDiv).attr('id','code-tips');
 				$(codeTipsDiv).append('<h3>Python Tips</h3>');
+				$(codeTipsDiv).append('<div class="alert alert-danger code-tip alert-dismissible" style="font-size:13px"><div class="close-button"><span class="glyphicon glyphicon-remove"></span></div><span class="suggestion">If you want to define a function you need to end the line with a colon.</span></div>');
+				$(codeTipsDiv).append('<div class="alert alert-warning code-tip alert-dismissible" style="font-size:13px"><div class="close-button"><span class="glyphicon glyphicon-remove"></span></div><span class="suggestion">You wrote <code>"5"</code> but may want <code>5</code>. Remove the quotes if you want to use the number 5.</span></div>');
+				$(codeTipsDiv).append('<div class="alert alert-info code-tip alert-dismissible" style="font-size:13px"><div class="close-button"><span class="glyphicon glyphicon-remove"></span></div><span class="suggestion">If you want to iterate over the items in your list, you can just write <pre>for item in list:<br>  print item</pre></span></div>');
 			}
 
 			$('.CodeMirror-lines pre').each(function(i, elem){
